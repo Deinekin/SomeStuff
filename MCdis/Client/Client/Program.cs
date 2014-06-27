30adfc71e3713bc5965c20f60721bea7dde36701
Deinekin <svyadey@mail.ru>
2014-06-27 07:24:19 +0400

    client-server task

---
+++ MCdis/Client/Client/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    class Program
+    {
+        private static void CopyFiles(string SourcePath, string DestinationPath)
+        {
+            Directory.CreateDirectory(DestinationPath);
+
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath));
+        }
+
+        private static void Connect(String server, String message)
+        {
+            try
+            {
+                var port = 32000;
+                var client = new TcpClient(server, port);
+
+                var data = System.Text.Encoding.ASCII.GetBytes(message);
+                var stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                stream.Flush();
+
+                data = new Byte[256];
+
+                var responseData = string.Empty;
+
+                var bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an IP address (you need 127.0.0.1 for success, but you can try anything else): \n");
+            var address = Console.ReadLine();
+            
+            var match = Regex.Match(address, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+
+            if (match.Success && address == "127.0.0.1")
+            {
+                Console.WriteLine("\n We have just received a new message!\n");
+                Connect("127.0.0.1", "Hey, i have to update my folder structure\n");
+                string pathServer = @"c:\\newDir";
+                string pathClient = @"c:\\newDir1";
+
+                if (Directory.Exists(pathClient))
+                    Directory.Delete(pathClient, true);
+
+                CopyFiles(pathServer, pathClient);
+                Console.WriteLine("We have just updated client's folder structure");
+            }
+            else
+            {
+               if (match.Success && address != "127.0.0.1")
+                   Console.WriteLine("I dont know this ip address");
+               else
+                   Console.WriteLine("Something strange... might be Elven");
+            }
+        }
+    }
+}
---
+++ MCdis/Server/Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        private static void ProcessingRequest(object client_obj)
+        {
+            var bytes = new Byte[256];
+            string data = null;
+
+            TcpClient client = client_obj as TcpClient;
+
+            data = null;
+            NetworkStream stream = client.GetStream();
+
+            int i;
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+            }
+            client.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                var port = 32000;
+                var localAddr = IPAddress.Parse("127.0.0.1");
+                var counter = 0;
+                server = new TcpListener(localAddr, port);
+
+                server.Start();
+
+                while (true)
+                {
+                    Console.Write("\nWaiting for a connection... ");
+                    ThreadPool.QueueUserWorkItem(ProcessingRequest, server.AcceptTcpClient());
+                    counter++;
+                    Console.Write("\nConnection №" + counter.ToString() + "!");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
